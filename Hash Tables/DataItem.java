2e57bc207e9086abfed6ecaa4ab7fcc65156dccf
Olivia Watt <msoliviawatt@gmail.com>
2023-03-08 22:00:32 -0500

    hashbrowns

--- Hash Tables/DataItem.java
+++ Hash Tables/DataItem.java
@@ -5,11 +5,13 @@ public class DataItem implements Comparable {
     private int num;
     private String word;
 
+    //constructor
     public DataItem (int n, String s) {
         num = n;
         word = s;
     }
 
+    //getter methods
     public int getNum() {
         return num;
     }
@@ -18,23 +20,37 @@ public class DataItem implements Comparable {
         return word;
     }
 
+    //setter methods
     public void setNum(int n) {
-        num = n;
+        this.num = n;
     }
 
     public void setWord(String s) {
-        word = s;
+        this.word =s;
     }
 
+    //to string
     public String toString() {
         return num + " ";
     }
 
+    //sum of the ASCII values * index position
     public int hashCode() {
-        return null;
+        int sum = 0;
+        for(int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            sum += (int)(letter) * i;
+        }
+        return sum;
     }
 
+    //takes in an object which is casted to a data item
+    //negative if the sent in object's num is < the parameter
+    //positive if greater
+    //zero if equal
     public int compareTo(Object x) {
-        return Integer.compare(this.num, x.getNum());
+        DataItem item = (DataItem) x;
+        return Integer.compare(this.num, item.getNum());
     }
+
 }
